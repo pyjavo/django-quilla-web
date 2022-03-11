afb03659db0369a97debf12eda84eb8f69b82678
Sergio C. Orozco Torres <scot3004@gmail.com>
2022-03-11 07:15:42 -0500

    Organización de assets

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,21 @@
-(function($) {
+// Twemoji
+twemoji.parse(document.body);
 
-// prettyPhoto
-	jQuery(document).ready(function(){
-		jQuery('a[data-gal]').each(function() {
-			jQuery(this).attr('rel', jQuery(this).data('gal'));
-		});  	
-		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
-	}); 
+var img = div.querySelector('img');
 
-		
-})(jQuery);
+// note the div is preserved
+img.parentNode === div; // true
+
+img.src;        // https://twemoji.maxcdn.com/v/latest/72x72/2764.png
+img.alt;        // \u2764\uFE0F
+img.className;  // emoji
+img.draggable;  // false
+
+// Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-102964527-1', 'auto');
+    ga('send', 'pageview');
