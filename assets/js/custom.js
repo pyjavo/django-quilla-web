f01692b5160694310d0aff21cadba29a60d06990
Sergio C. Orozco Torres <scot3004@gmail.com>
2022-03-11 07:36:01 -0500

    Tweemoji no requiere custom js para funcionar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,3 @@
-// Twemoji
-twemoji.parse(document.body);
-
-var img = div.querySelector('img');
-
-// note the div is preserved
-img.parentNode === div; // true
-
-img.src;        // https://twemoji.maxcdn.com/v/latest/72x72/2764.png
-img.alt;        // \u2764\uFE0F
-img.className;  // emoji
-img.draggable;  // false
-
 // Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
